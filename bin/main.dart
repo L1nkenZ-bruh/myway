fd58448bf54fee029e0f771317d544f090eb3938
L1nkenZ-bruh <johanwarframe2@gmail.com>
2024-09-26 16:53:47 +0300

    three

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,9 @@
 import 'dart_application_1.dart';
 void main(){
   var vacok = Human(age: 24, gender: 'male', height: 185.5, name: 'Alek');
+  var m =Human(age: 23, gender: 'male', height: 195.5, name: 'Andrew');
   print(vacok);
+  print(m);
 }
 
 
