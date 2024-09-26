bc08848498eb69afb357e34dff2c371411a08e80
LinkenZ <30.dd@bk.ru>
2024-09-26 16:47:23 +0300

    second

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,7 @@
 import 'dart_application_1.dart';
 void main(){
-  var vacok = Human(age: 25, gender: 'male', height: 185.5, name: 'Alek');
+  var vacok = Human(age: 24, gender: 'male', height: 185.5, name: 'Alek');
   print(vacok);
-  print(Human.counter);
 }
-
 
 
